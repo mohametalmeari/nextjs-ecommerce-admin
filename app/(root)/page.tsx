af5785782b1d544d50f4b10656e7508945439a9d
mohametalmeari <mohamet.almeari@gmail.com>
2024-02-21 02:59:00 +0300

    Use store modal hook

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,19 @@
-const page = () => {
-  return <div>Root Page</div>;
+"use client";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
+
+const Page = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div className="p-4">Root Page</div>;
 };
 
-export default page;
+export default Page;
