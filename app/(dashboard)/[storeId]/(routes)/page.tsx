7885337ad5f24ec8af97f0a6bc79481bbe41e5c4
mohametalmeari <mohamet.almeari@gmail.com>
2024-02-21 14:17:16 +0300

    Redirect to dashboard after store creation & Rest database as follow: npx prisma migrate reset npx prisma generate npx prisma db push

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,5 +1,14 @@
-const Page = () => {
-  return <div>Dashboard</div>;
+import prismadb from "@/lib/prismadb";
+
+interface PageProps {
+  params: { storeId: string };
+}
+
+const Page: React.FC<PageProps> = async ({ params }) => {
+  const store = await prismadb.store.findFirst({
+    where: { id: params.storeId },
+  });
+  return <div>Dashboard - active store is {store?.name}</div>;
 };
 
 export default Page;
