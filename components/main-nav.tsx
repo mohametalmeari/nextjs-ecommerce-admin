0f02c82f6e32463ce4ea77f17e85edc1ce82f685
mohametalmeari <mohamet.almeari@gmail.com>
2024-02-21 14:44:02 +0300

    Create MainNav component

---
+++ components/main-nav.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { useParams, usePathname } from "next/navigation";
+
+const MainNav = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLElement>) => {
+  const pathname = usePathname();
+  const params = useParams();
+
+  const routes = [
+    {
+      href: "/test",
+      label: "Test",
+      active: pathname === "/test",
+    },
+  ];
+  return (
+    <nav>
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            route.active
+              ? "text-black dark:text-white"
+              : "text-muted-foreground"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default MainNav;
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,12 @@
 import { UserButton } from "@clerk/nextjs";
+import MainNav from "./main-nav";
 
 const Navbar = () => {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
         <div>{/* TODO: Store Switcher */}</div>
-        <div>{/* TODO: Routes */}</div>
+        <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           {/* TODO: Theme Switcher */}
           <UserButton afterSignOutUrl="/" />
