4b2c23f1d61c4492833d6919e015d45a595118cf
mohametalmeari <mohamet.almeari@gmail.com>
2024-02-21 13:50:07 +0300

    Create dashboard layout and page

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,33 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { storeId: string };
+}) {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  // .findFirst() is used instead of .findUnique() to avoid throwing an error if the store is not found
+  const store = await prismadb.store.findFirst({
+    where: { id: params.storeId, userId },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <>
+      {/* TODO: Navbar */}
+      {children}
+    </>
+  );
+}
