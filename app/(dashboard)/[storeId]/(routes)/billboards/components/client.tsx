58cb28df0125564d0a871f5af11c55180470a3cb
mohametalmeari <mohamet.almeari@gmail.com>
2024-02-25 02:19:24 +0300

    Refactor BillboardClient component to accept data prop

--- app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -3,10 +3,15 @@
 import { Button } from "@/components/ui/button";
 import Heading from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
+import { Billboard } from "@prisma/client";
 import { Plus } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 
-const BillboardClient = () => {
+interface BillboardClientProps {
+  data: Billboard[];
+}
+
+const BillboardClient: React.FC<BillboardClientProps> = ({ data }) => {
   const router = useRouter();
   const params = useParams();
 
@@ -14,7 +19,7 @@ const BillboardClient = () => {
     <>
       <div className="flex items-center justify-between">
         <Heading
-          title={`Billboards (${0})`}
+          title={`Billboards (${data.length})`}
           description="Manage billboards for your store"
         />
         <Button
@@ -26,7 +31,7 @@ const BillboardClient = () => {
           Add New
         </Button>
       </div>
-      <Separator /> 
+      <Separator />
     </>
   );
 };
