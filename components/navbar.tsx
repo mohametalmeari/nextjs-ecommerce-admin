be5c0f05a555ca87fbc4aa52ee63e73c995bd1a1
mohametalmeari <mohamet.almeari@gmail.com>
2024-02-26 23:01:43 +0300

    Implement dark mode

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import MainNav from "./main-nav";
 import StoreSwitcher from "./store-switcher";
 import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from "./theme-toggle";
 
 const Navbar = async () => {
   const { userId } = auth();
@@ -19,7 +20,7 @@ const Navbar = async () => {
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
-          {/* TODO: Theme Switcher */}
+          <ThemeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
